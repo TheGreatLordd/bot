a1be8068e82471dec82d28b3d9d9614116f4439a
Jacob <auburn300jtn@proton.me>
2026-04-09 00:29:37 -0700

    first commit

---
+++ src/events/ready.js
@@ -0,0 +1,6 @@
+module.exports = {
+  name: "clientReady",
+  async execute(client) {
+    console.log("The bot is now online!");
+  },
+};
---
+++ src/index.js
@@ -0,0 +1,26 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+require("dotenv").config();
+
+const fs = require("fs");
+
+
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
+});
+
+const eventFiles = fs.readdirSync("./src/events").filter((file) => file.endsWith(".js"));
+const functions = fs.readdirSync("./src/functions").filter((file) => file.endsWith(".js"));
+const prefixFolders = fs.readdirSync("./src/prefix").filter((file) => file.endsWith(".js"));
+const commandsFolders = fs.readdirSync("./src/commands");
+
+(async () => {
+  for (file of functions) {
+    require(`./functions/${file}`)(client);
+  }
+
+  client.handleEvents(eventFiles, "./src/events");
+  
+  client.login(process.env.TOKEN);
+})();
+
